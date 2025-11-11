810432cc87acad22b541b9597cf55be6e3205716
rafleeea <rafli.10522118@mahasiswa.unikom.ac.id>
2025-11-11 21:07:09 +0700

    add packages

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,7 @@ app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:3000",],
+    origin: ["http://localhost:5173",],
     credentials: true, 
   })
 );;
