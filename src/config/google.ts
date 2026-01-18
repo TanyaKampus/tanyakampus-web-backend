346cfaf3c046c5cf412aaa1bca2b54c0a4f642a9
rafliAAA <raflialamsyah184@gmail.com>
2026-01-19 00:11:45 +0700

    refactor: move Google OAuth2 redirect URI to environment variable

--- src/config/google.ts
+++ src/config/google.ts
@@ -3,14 +3,12 @@ import { google } from "googleapis";
 const oauth2Client = new google.auth.OAuth2(
   process.env.GOOGLE_CLIENT_ID,
   process.env.GOOGLE_CLIENT_SECRET,
-  "http://localhost:5000/api/auth/google/callback"
+  process.env.GOOGLE_REDIRECT_URI,
 );
 
 const scopes = [
   "https://www.googleapis.com/auth/userinfo.profile",
   "https://www.googleapis.com/auth/userinfo.email",
 ];
-
-
 
 export { oauth2Client, scopes };
